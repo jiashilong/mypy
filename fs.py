80798e81c59a2588ec634ab106cbf413ac467cb7
jarry <jiashilong_syit@163.com>
2016-01-07 23:48:37 +0800

    fist commit

---
+++ fs.py
@@ -0,0 +1,8 @@
+def fmax(a):
+	return max(a)
+
+def fmin(a):
+	return min(a)
+
+def flen(a):
+	return len(a)
---
+++ hello_world.py
@@ -0,0 +1,5 @@
+import fs
+
+if __name__ == '__main__':
+	print 'hello world'
+	print fs.fmax([1,2,3])
